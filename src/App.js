4d5ee99c3d6b409223849caee301c98171b86ad7
dam num <47239352+viprocket1@users.noreply.github.com>
2022-02-10 10:49:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>gameplex</h1>
+      <h2>all your crypto games at one place</h2>
+    </div>
+  );
+}
